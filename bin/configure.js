9951b7422b72e8bbf5f2d540cf70a321034fdfb1
Yoanm <yoanm@users.noreply.github.com>
2021-09-26 11:55:26 +0200

    test

--- bin/configure.js
+++ bin/configure.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports,@typescript-eslint/no-var-requires
 const { execSync } = require("child_process");
-console.log('configure');
-execSync("yarn install --prod", { stdio: "inherit" });
+console.log('configure'+ `(${__dirname})`);
+execSync(`cd ${__dirname}/../ && yarn install --prod`, { stdio: "inherit" });
 console.log('configure - ok');
